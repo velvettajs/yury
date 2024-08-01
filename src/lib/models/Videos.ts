1c60be84aefc52331f767f2f3e85371ed38bfd8c
osint <osintaxis@gmail.com>
2024-07-31 21:50:59 -0500

    Updating videos model

--- src/lib/models/Videos.ts
+++ src/lib/models/Videos.ts
@@ -1,11 +1,18 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, text, integer, uuid } from 'drizzle-orm/pg-core';
 import { tags } from './Tags.js';
 
 export const videos = pgTable('videos', {
 	id: uuid('id').primaryKey().defaultRandom(),
-	x_url: text('x_url'),
-	url: text('url'),
+	title: text('title').notNull(),
+	description: text('description').notNull(),
+	url: text('url').notNull().unique(),
 	preview: text('preview').notNull(),
+	image: text('image').notNull(),
+	duration: text('duration').notNull(),
+	hls: text('hls').notNull(),
+	low: text('low').notNull(),
+	high: text('high').notNull(),
+	views: integer('views').notNull(),
 	tag: uuid('tag')
 		.notNull()
 		.references(() => tags.id)
